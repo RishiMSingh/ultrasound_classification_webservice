a4455c7a4488f94f90e1839b724100f65eb07de9
Rishi Singh <43751459+RishiMSingh@users.noreply.github.com>
2025-08-03 21:55:07 -0400

    webservice integration

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+import io
+
+app = FastAPI()
+
+# Allow frontend (Streamlit) access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load your .keras model
+model = load_model("resnet50_ultrasound_model.keras")
+
+# Class names
+class_names = ['bad', 'good']
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    img = Image.open(io.BytesIO(contents)).convert("RGB")
+    img_array = np.array(img)
+    img_resized = cv2.resize(img_array, (224, 224))
+
+    test_image = image.img_to_array(img_resized)
+    test_image = np.expand_dims(test_image, axis=0)
+
+    # Prediction logic
+    result = model.predict(test_image)
+    probability = tf.sigmoid(result).numpy()[0][0]
+    predicted_class = "good" if probability > 0.5 else "bad"
+
+    if predicted_class == "bad":
+        probability = 1 - probability
+
+    return {
+        "prediction": predicted_class,
+        "probability": float(probability),
+    }
